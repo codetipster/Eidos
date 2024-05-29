97387baf4c4c6b5c3fcd7002c9e2a3d6d27766ba
codetipster <talk2samuelnzekwe@gmail.com>
2024-05-29 02:50:25 +0200

    authentication with next-auth and google provider+ header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import FormGenerator from './form-generator'
+import Header from '@/components/ui/header'
 
 export default function Home() {
   return (
+    <>
+    <Header />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <FormGenerator />
       
     </main>
+    </>
   )
 }
